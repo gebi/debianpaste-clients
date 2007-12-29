2695dd119035d5f6f703441ea0ab39f1451ca881
Michael Gebetsroither <michael.geb@gmx.at>
2007-12-29 19:35:59 +0100

    first try

--- paste.py
+++ paste.py
@@ -4,17 +4,65 @@ import sys
 import xmlrpclib
 import optparse
 
-SERVER='http://localhost/paste.pl/server.pl'
+SERVER='http://paste.debian.net'
 
-if __name__ == "__main__":
-    parser = optparse.OptionParser(usage="usage: %prog [options] <code>")
-    parser.add_option('-n', '--name', default='anonymous', help="Name of poster")
-    (opts, args) = parser.parse_args()
+def createProxy():
+    return xmlrpclib.ServerProxy(SERVER)
+
+def actionAddPaste(opts, args):
+    cmd = args.pop(0)
+
+    server = createProxy()
     code = args
     if len(args) == 0:
         code = [ i.strip() for i in sys.stdin.readlines() ]
     code = '\n'.join(code)
+    result = server.paste.addPaste(code, opts.name, opts.expire, opts.lang)
+    print result
 
-    server = xmlrpclib.ServerProxy(SERVER)
-    result = server.paste.addPaste(code, opts.name)
-    print result.statusmessage
+def actionDelPaste(opts, args):
+    cmd = args.pop(0)
+    digest = args.pop(0)
+
+    server = createProxy()
+    result = server.paste.deletePaste(digest)
+
+def actionGetPaste(opts, args):
+    cmd = args.pop(0)
+    id = args.pop(0)
+
+    server = createProxy()
+    result = server.paste.getPaste(id)
+    print result
+
+def actionGetLangs(opts, args):
+    cmd = args.pop(0)
+
+    server = createProxy()
+    result = server.paste.getLanguages()
+    print result
+
+
+##
+# MAIN
+##
+if __name__ == "__main__":
+    actions = {'add'     : actionAddPaste,
+               'del'     : actionDelPaste,
+               'rm'      : actionDelPaste,
+               'get'     : actionGetPaste,
+               'getlangs': actionGetLangs,
+               'langs'   : actionGetLangs }
+    parser = optparse.OptionParser(usage="usage: %prog [options] ACTION <args>")
+    parser.add_option('-n', '--name', default='anonymous', help="Name of poster")
+    parser.add_option('-e', '--expire', type=int, default=72, metavar='HOURS',
+            help='Time at wich the paste should expire')
+    parser.add_option('-l', '--lang', default='plain', help='Type of highlighting')
+    (opts, args) = parser.parse_args()
+
+    if len(args) == 0:
+        parser.error('Please provide me with an action')
+    elif args[0] in actions:
+        actions[args[0]](opts, args)
+    else:
+        parser.error('Unknown action: %s' % args[0])
