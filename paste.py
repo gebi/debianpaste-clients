24415d5a607a74bbbf3fe0abdbb9e4d0a730a7ef
Michael Gebetsroither <michael.geb@gmx.at>
2007-12-29 23:46:46 +0100

    next step

--- paste.py
+++ paste.py
@@ -4,41 +4,40 @@ import sys
 import xmlrpclib
 import optparse
 
-SERVER='http://paste.debian.net'
 
-def createProxy():
-    return xmlrpclib.ServerProxy(SERVER)
+def createProxy(opts):
+    return xmlrpclib.ServerProxy(opts.server)
 
 def actionAddPaste(opts, args):
     cmd = args.pop(0)
 
-    server = createProxy()
+    server = createProxy(opts)
     code = args
     if len(args) == 0:
         code = [ i.strip() for i in sys.stdin.readlines() ]
     code = '\n'.join(code)
-    result = server.paste.addPaste(code, opts.name, opts.expire, opts.lang)
+    result = server.paste.addPaste(code, opts.name, opts.expire * 3600, opts.lang)
     print result
 
 def actionDelPaste(opts, args):
     cmd = args.pop(0)
     digest = args.pop(0)
 
-    server = createProxy()
+    server = createProxy(opts)
     result = server.paste.deletePaste(digest)
 
 def actionGetPaste(opts, args):
     cmd = args.pop(0)
     id = args.pop(0)
 
-    server = createProxy()
+    server = createProxy(opts)
     result = server.paste.getPaste(id)
     print result
 
 def actionGetLangs(opts, args):
     cmd = args.pop(0)
 
-    server = createProxy()
+    server = createProxy(opts)
     result = server.paste.getLanguages()
     print result
 
@@ -57,7 +56,9 @@ if __name__ == "__main__":
     parser.add_option('-n', '--name', default='anonymous', help="Name of poster")
     parser.add_option('-e', '--expire', type=int, default=72, metavar='HOURS',
             help='Time at wich the paste should expire')
-    parser.add_option('-l', '--lang', default='plain', help='Type of highlighting')
+    parser.add_option('-l', '--lang', default='plain', help='Type of language to highlight')
+    parser.add_option('-s', '--server', default='http://paste.snow-crash.org/server.pl',
+            help='Paste server')
     (opts, args) = parser.parse_args()
 
     if len(args) == 0:
